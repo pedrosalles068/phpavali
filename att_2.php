34abd6d6c0ae9c7972812120bf85987466b24e80
Deagleoh <vralnelas1@gmail.com>
2024-04-02 20:53:53 -0300

    att_2

--- att_2.php
+++ att_2.php
@@ -1,3 +1,25 @@
 <?php
+function Divisão($numero) {
+    echo "Divisão numero\n";
+    for ($i = 1; $i <= $numero; $i++) {
+        if ($numero % $i ==0 ){
+            echo "$i ";
+        }
+    }
+}
+
+function lei_tura() {
+    while (true){
+        $numero = readline("Escreve o numero inteiro: ");
+        if(is_numeric($numero) && $numero > 0 && ctype_digit ($numero)){
+            return (int)$numero;            
+        } else {
+            echo "Numero não inteiro";
+        }
+    }    
+}
+
+$numero = lei_tura();
+Divisão($numero);
 
 
