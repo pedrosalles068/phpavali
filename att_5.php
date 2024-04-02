b316b0c4fdc0a179912fc274f0118ac264ac5cb5
selvatixd <mathselvati@gmail.com>
2024-04-02 20:32:59 -0300

    att5

--- att_5.php
+++ att_5.php
@@ -1,2 +1,21 @@
 <?php
+$numeroAleatorio = rand(1, 100);
+echo "Adivinhe o número entre 1 a 100!\n";
 
+while (true) {
+    $acertar = readline("Digite o número:\n ");
+    if (!is_numeric($acertar)) {
+        echo "Número incorreto, digite outro:\n";
+        continue;
+    }
+    $acertar = (int)$acertar;
+    if ($acertar < $numeroAleatorio) {
+        echo "Dica: Seu número é maior!\n";
+    } elseif ($acertar > $numeroAleatorio) {
+        echo "Dica: Seu número é menor!\n";
+    } else {
+        echo "Billll!!, Acertou o número\n";
+        break;
+    }
+}
+?>
