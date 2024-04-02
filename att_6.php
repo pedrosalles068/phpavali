e7286658edc54dfb5d39a56a0af2c45c2fdb2cdf
selvatixd <mathselvati@gmail.com>
2024-04-02 20:53:09 -0300

    att6

--- att_6.php
+++ att_6.php
@@ -1,2 +1,21 @@
 <?php
+function bubbleSort($array)
+{
+    $n = count($array);
+    for ($i = 0; $i < $n - 1; $i++) {
+        for ($j = 0; $j < $n - $i - 1; $j++) {
+            if ($array[$j] > $array[$j + 1]) {
+                $temp = $array[$j];
+$array[$j] = $array[$j + 1];
+$array[$j + 1] = $temp;
+}}}
+return $array;
+}
+$array = [64, 34, 25, 12, 22, 11, 90];
+echo "Original: ";
+print_r($array);
 
+echo "Ordenado com BubbleSort: ";
+$sortedArray = bubbleSort($array);
+print_r($sortedArray);
+?>
