a6c27e0ec98d44770ab53b58d4afd9a32df0b97c
Deagleoh <vralnelas1@gmail.com>
2024-04-02 20:40:44 -0300

    att_1

--- att_1.php
+++ att_1.php
@@ -1,2 +1,23 @@
 <?php
+function tabuada($numero) {
+    echo "Tabuda de numero\n";
+    for ($i = 1; $i <=10;$i++) {
+        $result = ($numero * $i);
+        //print_r($numero);
+        echo "{$numero} x {$i} = {$result}".PHP_EOL;
+    }
+}
 
+function lei_tura() {
+    while (true){
+        $numero = readline("Escreve o numero inteiro: ");
+        if(is_numeric($numero) && $numero > 0 && ctype_digit ($numero)){
+            return (int)$numero;            
+        } else {
+            echo "Numero não inteiro";
+        }
+    }    
+}
+
+$numero = lei_tura();
+tabuada($numero);
