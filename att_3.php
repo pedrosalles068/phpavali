7337be20fe2d9209dad6ebae828ed7567892870f
brenotrombini <brenovanonni03@hotmail.com>
2024-04-02 20:12:15 -0300

    att3

--- att_3.php
+++ att_3.php
@@ -1,2 +1,17 @@
 <?php
+soma = 0
+contador = 0
+
+numero = int(input("Escreva um número inteiro (negativo para terminar): "))
+
+while numero >= 0:
+    soma += numero
+    contador += 1
+    numero = int(input("Escreva um número inteiro (negativo para terminar): "))
+
+if contador > 0:
+    media = soma / contador
+    print("A média dos valores positivos é:", media)
+else:
+    print("Nenhum número positivo foi colocado.")
 
