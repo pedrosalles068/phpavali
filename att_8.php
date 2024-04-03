473295d403546f0fff45ee6d404d38d02a53cab9
Deagleoh <vralnelas1@gmail.com>
2024-04-02 21:16:44 -0300

    att_8

--- att_8.php
+++ att_8.php
@@ -1,2 +1,20 @@
 <?php
+function vacaquefala($mensagem) {
+    $vaca = "
+    $mensagem
+     ________________________________________
+    /                                         \
+     $mensagem        
+    \                                         /
+     ----------------------------------------
+        \   ^__^
+         \  (oo)\_______
+            (__)\       )\/\
+                ||----w |
+                ||     ||";
+    echo "$vaca";
+}
 
+$mensagem = readline("Digite alguma coisa: ").PHP_EOL;
+
+vacaquefala($mensagem);
