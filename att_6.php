57d3886a132656edba376c0fb9e997bed81f9278
selvatixd <mathselvati@gmail.com>
2024-04-02 21:26:22 -0300

    att6

--- att_6.php
+++ att_6.php
@@ -1,21 +1,29 @@
 <?php
-function bubbleSort($arr)
-{
-    $n = count($arr);
-    for ($i = 0; $i < $n - 1; $i++) {
-        for ($obs = 0; $obs < $n - $i - 1; $obs++) {
-            if ($arr[$obs] > $arr[$obs + 1]) {
-                $tempo = $arr[$obs];
-$arr[$obs] = $arr[$obs + 1];
-$arr[$obs + 1] = $tempo;
-}}}
-return $arr;
-}
-$arr = [64, 34, 25, 12, 22, 11, 90];
-echo "Original: ";
-print_r($arr);
 
-echo "Ordenado com BubbleSort: ";
-$sortedArr = bubbleSort($arr);
-print_r($sortedArr);
+function bubbleSort($numero)
+{
+    $n = count($numero);
+    $trocou = true;
+    while ($trocou) {
+        $trocou = false;
+        for ($i = 0; $i < $n - 1; $i++) {
+            if ($numero[$i] > $numero[$i + 1]) {
+                // Troca de posições
+                $tempo = $numero[$i];
+                $numero[$i] = $numero[$i + 1];
+                $numero[$i + 1] = $tempo;
+                $trocou = true;
+            }
+        }}
+    return $numero;
+}
+
+$numero = [64, 34, 25, 12, 22, 11, 90];
+
+echo "Array original: ";
+print_r($numero);
+
+echo "Array ordenado com Bubble Sort: ";
+$sortedArray = bubbleSort($numero);
+
 ?>
